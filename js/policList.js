6e8d232425e6181a7037878e55f25f5e257cf1ac
minkyong00 <minkyong131@gmail.com>
2025-08-01 19:51:15 +0900

    검색 버튼 클릭 시 ui 출력 추가

--- js/policList.js
+++ js/policList.js
@@ -1,1 +1,18 @@
 // 파출소 목록 가져오는 js
+
+// 검색창 input
+const searchTxt = document.querySelector("#searchTxt");
+// 검색창 버튼
+const searchBtn = document.querySelector("#searchBtn");
+// 검색창 파출소 div
+const policeListWrap = document.querySelector("#policeListWrap");
+
+// 검색창 input 값
+searchTxt.addEventListener("input", e => {
+    console.log(e.target.value);
+})
+
+searchBtn.addEventListener("click", e => {
+    console.log("클릭");
+    policeListWrap.innerHTML = '<ul id="policeUl"></ul>';
+});
