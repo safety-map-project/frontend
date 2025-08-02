72e75d817859e14c0e5634980013a65650897eaf
minkyong00 <minkyong131@gmail.com>
2025-08-02 23:18:47 +0900

    주소 입력하고 버튼을 클릭해야 검색 리스트 출력 추가

--- js/policList.js
+++ js/policList.js
@@ -7,12 +7,25 @@ const searchBtn = document.querySelector("#searchBtn");
 // 검색창 파출소 div
 const policeListWrap = document.querySelector("#policeListWrap");
 
-// 검색창 input 값
-searchTxt.addEventListener("input", e => {
-    console.log(e.target.value);
-})
-
+// 주소 입력하고 검색 버튼 클릭 시
 searchBtn.addEventListener("click", e => {
+
+    // 입력창 값
+    const input = searchTxt.value.trim();
+    console.log(input);
+
+    // 주소 입력하지 않으면 alert창 출력
+    if(!input){
+        alert("주소를 입력하세요!");
+        return;
+    }
+
+    
+    // policeListWrap div 표시됨
+    policeListWrap.style.display = "block";
+
     console.log("클릭");
+
+    // police 검색 리스트 ui html 추가
     policeListWrap.innerHTML = '<ul id="policeUl"></ul>';
 });
